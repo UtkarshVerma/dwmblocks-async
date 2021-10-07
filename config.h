060a64bfcecee5d5b152d0098a312f70baae7125
Utkarsh Verma <utkarshverma@protonmail.com>
2021-10-07 11:00:49 +0530

    Use signals above 16 as powerline status uses the ones below.

--- config.h
+++ config.h
@@ -10,13 +10,13 @@ typedef struct {
 } Block;
 
 static Block blocks[] = {
-	{"sb-mail", 1800, 1},
-	{"sb-music", 0, 2},
-	{"sb-disk", 1800, 4},
-	{"sb-memory", 10, 3},
-	{"sb-loadavg", 10, 9},
-	{"sb-volume", 0, 5},
-	{"sb-battery", 5, 6},
-	{"sb-date", 20, 7},
-	{"sb-network", 5, 8}
+	{"sb-mail", 1800, 17},
+	{"sb-music", 0, 18},
+	{"sb-disk", 1800, 19},
+	{"sb-memory", 10, 20},
+	{"sb-loadavg", 10, 21},
+	{"sb-volume", 0, 22},
+	{"sb-battery", 5, 23},
+	{"sb-date", 20, 24},
+	{"sb-network", 5, 25}
 };
