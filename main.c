647080cbededd01aa78476901be7929e5e20bb6c
Utkarsh Verma <utkarshverma@protonmail.com>
2022-08-27 09:27:26 +0200

    Add .clang-format

--- main.c
+++ main.c
@@ -12,12 +12,12 @@
 #define LEN(arr) (sizeof(arr) / sizeof(arr[0]))
 #define MAX(a, b) (a > b ? a : b)
 #define BLOCK(cmd, interval, signal) \
-	{ "echo \"$(" cmd ")\"", interval, signal }
+    { "echo \"$(" cmd ")\"", interval, signal }
 
 typedef const struct {
-	const char* command;
-	const unsigned int interval;
-	const unsigned int signal;
+    const char *command;
+    const unsigned int interval;
+    const unsigned int signal;
 } Block;
 #include "config.h"
 
@@ -35,7 +35,7 @@ typedef const struct {
 #define LEADING_DELIMITER 0
 #endif
 
-static Display* dpy;
+static Display *dpy;
 static int screen;
 static Window root;
 static unsigned short statusContinue = 1;
@@ -48,265 +48,256 @@ static int execLock = 0;
 
 // Longest UTF-8 character is 4 bytes long
 static char outputs[LEN(blocks)][CMDLENGTH * 4 + 1 + CLICKABLE_BLOCKS];
-static char statusBar[2][LEN(blocks) * (LEN(outputs[0]) - 1) + (LEN(blocks) - 1 + LEADING_DELIMITER) * (LEN(DELIMITER) - 1) + 1];
+static char
+    statusBar[2]
+             [LEN(blocks) * (LEN(outputs[0]) - 1) +
+              (LEN(blocks) - 1 + LEADING_DELIMITER) * (LEN(DELIMITER) - 1) + 1];
 
 void (*writeStatus)();
 
 int gcd(int a, int b) {
-	int temp;
-	while (b > 0) {
-		temp = a % b;
-		a = b;
-		b = temp;
-	}
-	return a;
+    int temp;
+    while (b > 0) {
+        temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
 }
 
-void closePipe(int* pipe) {
-	close(pipe[0]);
-	close(pipe[1]);
+void closePipe(int *pipe) {
+    close(pipe[0]);
+    close(pipe[1]);
 }
 
-void execBlock(int i, const char* button) {
-	// Ensure only one child process exists per block at an instance
-	if (execLock & 1 << i)
-		return;
-	// Lock execution of block until current instance finishes execution
-	execLock |= 1 << i;
+void execBlock(int i, const char *button) {
+    // Ensure only one child process exists per block at an instance
+    if (execLock & 1 << i) return;
+    // Lock execution of block until current instance finishes execution
+    execLock |= 1 << i;
 
-	if (fork() == 0) {
-		close(pipes[i][0]);
-		dup2(pipes[i][1], STDOUT_FILENO);
-		close(pipes[i][1]);
+    if (fork() == 0) {
+        close(pipes[i][0]);
+        dup2(pipes[i][1], STDOUT_FILENO);
+        close(pipes[i][1]);
 
-		if (button)
-			setenv("BLOCK_BUTTON", button, 1);
-		execl("/bin/sh", "sh", "-c", blocks[i].command, (char*)NULL);
-		exit(EXIT_FAILURE);
-	}
+        if (button) setenv("BLOCK_BUTTON", button, 1);
+        execl("/bin/sh", "sh", "-c", blocks[i].command, (char *)NULL);
+        exit(EXIT_FAILURE);
+    }
 }
 
 void execBlocks(unsigned int time) {
-	for (int i = 0; i < LEN(blocks); i++)
-		if (time == 0 || (blocks[i].interval != 0 && time % blocks[i].interval == 0))
-			execBlock(i, NULL);
+    for (int i = 0; i < LEN(blocks); i++)
+        if (time == 0 ||
+            (blocks[i].interval != 0 && time % blocks[i].interval == 0))
+            execBlock(i, NULL);
 }
 
-int getStatus(char* new, char* old) {
-	strcpy(old, new);
-	new[0] = '\0';
+int getStatus(char *new, char *old) {
+    strcpy(old, new);
+    new[0] = '\0';
 
-	for (int i = 0; i < LEN(blocks); i++) {
+    for (int i = 0; i < LEN(blocks); i++) {
 #if LEADING_DELIMITER
-		if (strlen(outputs[i]))
+        if (strlen(outputs[i]))
 #else
-		if (strlen(new) && strlen(outputs[i]))
+        if (strlen(new) && strlen(outputs[i]))
 #endif
-			strcat(new, DELIMITER);
-		strcat(new, outputs[i]);
-	}
-	return strcmp(new, old);
+            strcat(new, DELIMITER);
+        strcat(new, outputs[i]);
+    }
+    return strcmp(new, old);
 }
 
 void updateBlock(int i) {
-	char* output = outputs[i];
-	char buffer[LEN(outputs[0]) - CLICKABLE_BLOCKS];
-	int bytesRead = read(pipes[i][0], buffer, LEN(buffer));
+    char *output = outputs[i];
+    char buffer[LEN(outputs[0]) - CLICKABLE_BLOCKS];
+    int bytesRead = read(pipes[i][0], buffer, LEN(buffer));
 
-	// Trim UTF-8 string to desired length
-	int count = 0, j = 0;
-	while (buffer[j] != '\n' && count < CMDLENGTH) {
-		count++;
+    // Trim UTF-8 string to desired length
+    int count = 0, j = 0;
+    while (buffer[j] != '\n' && count < CMDLENGTH) {
+        count++;
 
-		// Skip continuation bytes, if any
-		char ch = buffer[j];
-		int skip = 1;
-		while ((ch & 0xc0) > 0x80)
-			ch <<= 1, skip++;
-		j += skip;
-	}
+        // Skip continuation bytes, if any
+        char ch = buffer[j];
+        int skip = 1;
+        while ((ch & 0xc0) > 0x80) ch <<= 1, skip++;
+        j += skip;
+    }
 
-	// Cache last character and replace it with a trailing space
-	char ch = buffer[j];
-	buffer[j] = ' ';
+    // Cache last character and replace it with a trailing space
+    char ch = buffer[j];
+    buffer[j] = ' ';
 
-	// Trim trailing spaces
-	while (j >= 0 && buffer[j] == ' ')
-		j--;
-	buffer[j + 1] = 0;
+    // Trim trailing spaces
+    while (j >= 0 && buffer[j] == ' ') j--;
+    buffer[j + 1] = 0;
 
-	// Clear the pipe
-	if (bytesRead == LEN(buffer)) {
-		while (ch != '\n' && read(pipes[i][0], &ch, 1) == 1)
-			;
-	}
+    // Clear the pipe
+    if (bytesRead == LEN(buffer)) {
+        while (ch != '\n' && read(pipes[i][0], &ch, 1) == 1)
+            ;
+    }
 
 #if CLICKABLE_BLOCKS
-	if (bytesRead > 1 && blocks[i].signal > 0) {
-		output[0] = blocks[i].signal;
-		output++;
-	}
+    if (bytesRead > 1 && blocks[i].signal > 0) {
+        output[0] = blocks[i].signal;
+        output++;
+    }
 #endif
 
-	strcpy(output, buffer);
+    strcpy(output, buffer);
 
-	// Remove execution lock for the current block
-	execLock &= ~(1 << i);
+    // Remove execution lock for the current block
+    execLock &= ~(1 << i);
 }
 
 void debug() {
-	// Only write out if text has changed
-	if (!getStatus(statusBar[0], statusBar[1]))
-		return;
+    // Only write out if text has changed
+    if (!getStatus(statusBar[0], statusBar[1])) return;
 
-	write(STDOUT_FILENO, statusBar[0], strlen(statusBar[0]));
-	write(STDOUT_FILENO, "\n", 1);
+    write(STDOUT_FILENO, statusBar[0], strlen(statusBar[0]));
+    write(STDOUT_FILENO, "\n", 1);
 }
 
 int setupX() {
-	dpy = XOpenDisplay(NULL);
-	if (!dpy)
-		return 1;
+    dpy = XOpenDisplay(NULL);
+    if (!dpy) return 1;
 
-	screen = DefaultScreen(dpy);
-	root = RootWindow(dpy, screen);
-	return 0;
+    screen = DefaultScreen(dpy);
+    root = RootWindow(dpy, screen);
+    return 0;
 }
 
 void setRoot() {
-	// Only set root if text has changed
-	if (!getStatus(statusBar[0], statusBar[1]))
-		return;
+    // Only set root if text has changed
+    if (!getStatus(statusBar[0], statusBar[1])) return;
 
-	XStoreName(dpy, root, statusBar[0]);
-	XFlush(dpy);
+    XStoreName(dpy, root, statusBar[0]);
+    XFlush(dpy);
 }
 
 void signalHandler() {
-	struct signalfd_siginfo info;
-	read(signalFD, &info, sizeof(info));
-	unsigned int signal = info.ssi_signo;
+    struct signalfd_siginfo info;
+    read(signalFD, &info, sizeof(info));
+    unsigned int signal = info.ssi_signo;
 
-	switch (signal) {
-	case SIGALRM:
-		// Schedule the next timer event and execute blocks
-		alarm(timerTick);
-		execBlocks(timer);
+    switch (signal) {
+        case SIGALRM:
+            // Schedule the next timer event and execute blocks
+            alarm(timerTick);
+            execBlocks(timer);
 
-		// Wrap `timer` to the interval [1, `maxInterval`]
-		timer = (timer + timerTick - 1) % maxInterval + 1;
-		return;
-	case SIGUSR1:
-		// Update all blocks on receiving SIGUSR1
-		execBlocks(0);
-		return;
-	}
+            // Wrap `timer` to the interval [1, `maxInterval`]
+            timer = (timer + timerTick - 1) % maxInterval + 1;
+            return;
+        case SIGUSR1:
+            // Update all blocks on receiving SIGUSR1
+            execBlocks(0);
+            return;
+    }
 
-	for (int j = 0; j < LEN(blocks); j++) {
-		if (blocks[j].signal == signal - SIGRTMIN) {
-			char button[] = {'0' + info.ssi_int & 0xff, 0};
-			execBlock(j, button);
-			break;
-		}
-	}
+    for (int j = 0; j < LEN(blocks); j++) {
+        if (blocks[j].signal == signal - SIGRTMIN) {
+            char button[] = {'0' + info.ssi_int & 0xff, 0};
+            execBlock(j, button);
+            break;
+        }
+    }
 }
 
-void termHandler() {
-	statusContinue = 0;
-}
+void termHandler() { statusContinue = 0; }
 
 void setupSignals() {
-	sigset_t handledSignals;
-	sigemptyset(&handledSignals);
-	sigaddset(&handledSignals, SIGUSR1);
-	sigaddset(&handledSignals, SIGALRM);
+    sigset_t handledSignals;
+    sigemptyset(&handledSignals);
+    sigaddset(&handledSignals, SIGUSR1);
+    sigaddset(&handledSignals, SIGALRM);
 
-	// Append all block signals to `handledSignals`
-	for (int i = 0; i < LEN(blocks); i++)
-		if (blocks[i].signal > 0)
-			sigaddset(&handledSignals, SIGRTMIN + blocks[i].signal);
+    // Append all block signals to `handledSignals`
+    for (int i = 0; i < LEN(blocks); i++)
+        if (blocks[i].signal > 0)
+            sigaddset(&handledSignals, SIGRTMIN + blocks[i].signal);
 
-	// Create a signal file descriptor for epoll to watch
-	signalFD = signalfd(-1, &handledSignals, 0);
-	event.data.u32 = LEN(blocks);
-	epoll_ctl(epollFD, EPOLL_CTL_ADD, signalFD, &event);
+    // Create a signal file descriptor for epoll to watch
+    signalFD = signalfd(-1, &handledSignals, 0);
+    event.data.u32 = LEN(blocks);
+    epoll_ctl(epollFD, EPOLL_CTL_ADD, signalFD, &event);
 
-	// Block all realtime and handled signals
-	for (int i = SIGRTMIN; i <= SIGRTMAX; i++)
-		sigaddset(&handledSignals, i);
-	sigprocmask(SIG_BLOCK, &handledSignals, NULL);
+    // Block all realtime and handled signals
+    for (int i = SIGRTMIN; i <= SIGRTMAX; i++) sigaddset(&handledSignals, i);
+    sigprocmask(SIG_BLOCK, &handledSignals, NULL);
 
-	// Handle termination signals
-	signal(SIGINT, termHandler);
-	signal(SIGTERM, termHandler);
+    // Handle termination signals
+    signal(SIGINT, termHandler);
+    signal(SIGTERM, termHandler);
 
-	// Avoid zombie subprocesses
-	struct sigaction sa;
-	sa.sa_handler = SIG_DFL;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_NOCLDWAIT;
-	sigaction(SIGCHLD, &sa, 0);
+    // Avoid zombie subprocesses
+    struct sigaction sa;
+    sa.sa_handler = SIG_DFL;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_NOCLDWAIT;
+    sigaction(SIGCHLD, &sa, 0);
 }
 
 void statusLoop() {
-	// Update all blocks initially
-	raise(SIGALRM);
+    // Update all blocks initially
+    raise(SIGALRM);
 
-	struct epoll_event events[LEN(blocks) + 1];
-	while (statusContinue) {
-		int eventCount = epoll_wait(epollFD, events, LEN(events), -1);
-		for (int i = 0; i < eventCount; i++) {
-			unsigned short id = events[i].data.u32;
-			if (id < LEN(blocks))
-				updateBlock(id);
-			else
-				signalHandler();
-		}
+    struct epoll_event events[LEN(blocks) + 1];
+    while (statusContinue) {
+        int eventCount = epoll_wait(epollFD, events, LEN(events), -1);
+        for (int i = 0; i < eventCount; i++) {
+            unsigned short id = events[i].data.u32;
+            if (id < LEN(blocks))
+                updateBlock(id);
+            else
+                signalHandler();
+        }
 
-		if (eventCount != -1)
-			writeStatus();
-	}
+        if (eventCount != -1) writeStatus();
+    }
 }
 
 void init() {
-	epollFD = epoll_create(LEN(blocks));
-	event.events = EPOLLIN;
+    epollFD = epoll_create(LEN(blocks));
+    event.events = EPOLLIN;
 
-	for (int i = 0; i < LEN(blocks); i++) {
-		// Append each block's pipe to `epollFD`
-		pipe(pipes[i]);
-		event.data.u32 = i;
-		epoll_ctl(epollFD, EPOLL_CTL_ADD, pipes[i][0], &event);
+    for (int i = 0; i < LEN(blocks); i++) {
+        // Append each block's pipe to `epollFD`
+        pipe(pipes[i]);
+        event.data.u32 = i;
+        epoll_ctl(epollFD, EPOLL_CTL_ADD, pipes[i][0], &event);
 
-		// Calculate the max interval and tick size for the timer
-		if (blocks[i].interval) {
-			maxInterval = MAX(blocks[i].interval, maxInterval);
-			timerTick = gcd(blocks[i].interval, timerTick);
-		}
-	}
+        // Calculate the max interval and tick size for the timer
+        if (blocks[i].interval) {
+            maxInterval = MAX(blocks[i].interval, maxInterval);
+            timerTick = gcd(blocks[i].interval, timerTick);
+        }
+    }
 
-	setupSignals();
+    setupSignals();
 }
 
-int main(const int argc, const char* argv[]) {
-	if (setupX()) {
-		fprintf(stderr, "dwmblocks: Failed to open display\n");
-		return 1;
-	}
+int main(const int argc, const char *argv[]) {
+    if (setupX()) {
+        fprintf(stderr, "dwmblocks: Failed to open display\n");
+        return 1;
+    }
 
-	writeStatus = setRoot;
-	for (int i = 0; i < argc; i++)
-		if (!strcmp("-d", argv[i]))
-			writeStatus = debug;
+    writeStatus = setRoot;
+    for (int i = 0; i < argc; i++)
+        if (!strcmp("-d", argv[i])) writeStatus = debug;
 
-	init();
-	statusLoop();
+    init();
+    statusLoop();
 
-	XCloseDisplay(dpy);
-	close(epollFD);
-	close(signalFD);
-	for (int i = 0; i < LEN(pipes); i++)
-		closePipe(pipes[i]);
+    XCloseDisplay(dpy);
+    close(epollFD);
+    close(signalFD);
+    for (int i = 0; i < LEN(pipes); i++) closePipe(pipes[i]);
 
-	return 0;
+    return 0;
 }
