cbe5908cbce710b93661a27bf1656f45304722b8
Meakoala <141302175+Meakoala@users.noreply.github.com>
2023-09-12 20:23:46 +0700

    config

--- config.h
+++ config.h
@@ -2,5 +2,5 @@
 
 #define CLICKABLE_BLOCKS  1     // Enable clickability for blocks
 #define CMDLENGTH         45    // Trim block output to this length
-#define DELIMITER         "  "  // Delimiter string used to separate blocks
+#define DELIMITER         "|"   // Delimiter string used to separate blocks
 #define LEADING_DELIMITER 0     // Whether a leading separator should be used
