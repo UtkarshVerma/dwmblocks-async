b10e021105095a9a9df7ebd3171dad84295802bb
Meakoala <141302175+Meakoala@users.noreply.github.com>
2023-09-12 20:55:47 +0700

    config.c

--- config.c
+++ config.c
@@ -4,16 +4,7 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-mail",    600,  1 },
-    {"sb-music",   0,    2 },
-    {"sb-disk",    1800, 3 },
-    {"sb-memory",  10,   4 },
-    {"sb-loadavg", 5,    5 },
-    {"sb-mic",     0,    6 },
-    {"sb-record",  0,    7 },
-    {"sb-volume",  0,    8 },
-    {"sb-battery", 5,    9 },
-    {"sb-date",    1,    10},
+    {"/home/kia/Documents/Github/suckless/statusbar/date",    1,    1},
 };
 
 const unsigned short blockCount = LEN(blocks);
